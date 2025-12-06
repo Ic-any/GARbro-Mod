54a25c8e48699c8f4f94bc90aaddc1a4d07a82f9
Ic-any <92638461+Ic-any@users.noreply.github.com>
2025-12-06 20:32:31 +0800

    Update Program.cs

--- SchemeTool/Program.cs
+++ SchemeTool/Program.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -35,37 +35,41 @@ namespace SchemeTool
                 // Add scheme information here
 
 #if true
-                byte[] cb = File.ReadAllBytes(@"MEM_10014628_00001000.mem");
+                // 读取控制块文件
+                byte[] cb = File.ReadAllBytes(@"SICKLYDAYS.bin");  // 改为SICKLYDAYS.bin
                 var cb2 = MemoryMarshal.Cast<byte, uint>(cb);
                 for (int i = 0; i < cb2.Length; i++)
                     cb2[i] = ~cb2[i];
                 var cs = new GameRes.Formats.KiriKiri.CxScheme
                 {
-                    Mask = 0x000,
-                    Offset = 0x000,
-                    PrologOrder = new byte[] { 0, 1, 2 },
-                    OddBranchOrder = new byte[] { 0, 1, 2, 3, 4, 5 },
-                    EvenBranchOrder = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 },
+                    Mask = 0x2f2,  // 更新mask
+                    Offset = 0x2c5,  // 更新offset
+                    PrologOrder = new byte[] { 0, 2, 1 },  // 更新PrologOrder
+                    OddBranchOrder = new byte[] { 2, 5, 1, 0, 4, 3 },  // 更新OddBranchOrder
+                    EvenBranchOrder = new byte[] { 7, 6, 1, 4, 0, 5, 2, 3 },  // 更新EvenBranchOrder
                     ControlBlock = cb2.ToArray()
                 };
                 var crypt = new GameRes.Formats.KiriKiri.HxCrypt(cs);
-                crypt.RandomType = 0;
-                crypt.FilterKey = 0x0000000000000000;
-                crypt.NamesFile = "HxNames.lst";
-                var keyA1 = SoapHexBinary.Parse("0000000000000000000000000000000000000000000000000000000000000000").Value;
-                var keyA2 = SoapHexBinary.Parse("00000000000000000000000000000000").Value;
-                var keyB1 = SoapHexBinary.Parse("0000000000000000000000000000000000000000000000000000000000000000").Value;
-                var keyB2 = SoapHexBinary.Parse("00000000000000000000000000000000").Value;
+                crypt.RandomType = 0;  // 更新randtype
+                crypt.FilterKey = 0x975d6564faba3967;  // 更新filterkey
+                crypt.NamesFile = "SICKLYDAYS.lst";  // 改为SICKLYDAYS.lst
+                var keyA1 = SoapHexBinary.Parse("ea9a25a05369eef3dd9d7b24899145528804e5113d60446024d93a3400ae5307").Value;  // 更新key
+                var keyA2 = SoapHexBinary.Parse("d22226c626446fc2a4fc7075ba4822ba").Value;  // 更新nonce
+                var keyB1 = SoapHexBinary.Parse("ea9a25a05369eef3dd9d7b24899145528804e5113d60446024d93a3400ae5307").Value;  // 使用相同的key
+                var keyB2 = SoapHexBinary.Parse("d22226c626446fc2a4fc7075ba4822ba").Value;  // 使用相同的nonce
                 crypt.IndexKeyDict = new Dictionary<string, GameRes.Formats.KiriKiri.HxIndexKey>()
                 {
-                    { "data.xp3", new GameRes.Formats.KiriKiri.HxIndexKey { Key1 = keyA1, Key2 = keyA2 } },
-                    { "update.xp3", new GameRes.Formats.KiriKiri.HxIndexKey { Key1 = keyB1, Key2 = keyB2 } },
+                    { "SICKLYDAYS.bin", new GameRes.Formats.KiriKiri.HxIndexKey { Key1 = keyA1, Key2 = keyA2 } },  // 改为SICKLYDAYS.bin
+                    // 如果游戏有其他加密文件，可以添加到这里
+                    // { "update.xp3", new GameRes.Formats.KiriKiri.HxIndexKey { Key1 = keyB1, Key2 = keyB2 } },
                 };
+                
+                // 添加方案到已知方案列表，使用游戏名"SICKLYDAYS"作为标识符
+                scheme.KnownSchemes.Add("SICKLYDAYS", crypt);
 #else
                 GameRes.Formats.KiriKiri.ICrypt crypt = new GameRes.Formats.KiriKiri.XorCrypt(0x00);
 #endif
 
-                // scheme.KnownSchemes.Add("game title", crypt);
             }
 
             var gameMap = typeof(GameRes.FormatCatalog).GetField("m_game_map", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
@@ -73,8 +77,8 @@ namespace SchemeTool
 
             if (gameMap != null)
             {
-                // Add file name here
-                // gameMap.Add("game.exe", "game title");
+                // 添加游戏执行文件映射到方案名称
+                gameMap.Add("sickly_days.exe", "SICKLYDAYS");
             }
 
             // Save database
